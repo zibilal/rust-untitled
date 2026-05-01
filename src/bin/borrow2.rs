8c2f74b1530a138fa2887684ff5edebba2163ec0
zibilal <bmuhamm@gmail.com>
2026-05-01 09:39:36 +0700

    Borrowing

---
+++ src/bin/borrow2.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let mut s = String::from("hello");
+    {
+        let r1 = &mut s;
+    } // r1 goes out of scope here, so we can make a new reference
+      // no problems
+    let r2 = &mut s;
+}
